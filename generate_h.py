69049183ea1276e01aef9ddfbe218855f4743d8a
Bradley Emi <bemi@stanford.edu>
2017-03-20 14:19:17 -0700

    Adjust input params for generate and fix boundary problem

--- generate_h.py
+++ generate_h.py
@@ -1,69 +1,56 @@
 import numpy as np
 import random
+import cPickle as pickle
 
-N_VARS = 256
-N_CHECKS = 152
+M = 1024
 VAR_OUTDEG = 3
 CHECK_OUTDEG = 6
-N_BLOCKS = 16
-COUPLING_WIDTH = 4
+N_BLOCKS = 64
+COUPLING_WIDTH = 3
 
-def generate_coupled_h(n_vars, n_checks, var_outdeg, check_outdeg, n_blocks, coupling_width):
+def generate_coupled_h(M, var_outdeg, check_outdeg, n_blocks, coupling_width):
 
-    assert n_checks % (n_blocks + coupling_width - 1) == 0
-    n_checks_per_block = n_checks / (n_blocks + coupling_width - 1)
+    n_checks_per_block = M * var_outdeg / check_outdeg
     check_blocks = [range(n_checks_per_block*block_number, n_checks_per_block*(block_number+1)) \
-              for block_number in range(n_blocks + coupling_width -1)]
+              for block_number in range(n_blocks + coupling_width - 1)]
+    n_checks_eff = (n_blocks + coupling_width - 1)*n_checks_per_block
     
-    assert n_vars % n_blocks == 0
-    n_vars_per_block = n_vars / n_blocks
-    n_vars_eff = n_vars_per_block * (n_blocks + 2 * (coupling_width - 1)) 
-    var_blocks = [range(n_vars_per_block*block_number, n_vars_per_block*(block_number+1)) \
-                  for block_number in range(n_blocks + 2*(coupling_width - 1))]
+    var_blocks = [range(M*block_number, M*(block_number+1)) \
+                  for block_number in range(n_blocks)]
+    n_vars = M*n_blocks
     
-    H = np.zeros((n_checks, n_vars_eff), dtype=int)
+    H = np.zeros((n_checks_eff, n_vars), dtype=int)
 
-    assert len(var_blocks) - len(check_blocks) == coupling_width - 1
-    assert n_vars_per_block * var_outdeg == n_checks_per_block * check_outdeg 
-    
-    #edges_filled[i] = the number of edges variable i has filled
-    edges_filled = [0] * n_vars_eff
-    full_vars = set([])
+    #edges_filled[i] = the number of edges check i has filled
+    edges_filled = [0] * n_checks_eff
+    full_checks = set([])
 
-    for check_block_n, check_block in enumerate(check_blocks):
-        # Spread out the edges in a given check block
-        edges = range(n_checks_per_block * check_outdeg)
-
-        # The first check_outdeg edges go to the first check in the block, etc.
-        edge_to_check = {}
+    for var_block_n, var_block in enumerate(var_blocks):
+        print var_block_n
+        edges = range(M * var_outdeg)
+        edge_to_var = {}
         for edge_n, edge in enumerate(edges):
-            edge_to_check[edge_n] = check_block[edge_n / check_outdeg]
-        
-        # Get all possible variables (before accounting for full variables)
-        available_variables = set([])
-        # We can use all variables in the coupling_width blocks that we are coupling the checks to
-        for var_block in var_blocks[check_block_n : check_block_n + coupling_width]:
-            available_variables = available_variables.union(set(var_block))
-        # Get rid of full variables
-        available_variables = available_variables.difference(full_vars)     
+            edge_to_var[edge_n] = var_block[edge_n / var_outdeg]
 
-        # Shuffle the edges and assign them one at a time
+        available_checks = set([])
+        for check_block in check_blocks[var_block_n : var_block_n + coupling_width]:
+            available_checks = available_checks.union(set(check_block))
+        available_checks = available_checks.difference(full_checks)
+
         for edge in np.random.permutation(edges):
-            check = edge_to_check[edge]
-            # Choose a variable from available variables randomly, assign an edge. If an edge already exists pick again
+            var = edge_to_var[edge]
             while True:
-                var = np.random.choice(list(available_variables))
+                check = np.random.choice(list(available_checks))
                 if H[check, var] != 1:
                     H[check, var] = 1
                     break
-            edges_filled[var] += 1
-            if edges_filled[var] == var_outdeg:
-                available_variables.remove(var)
-                full_vars.add(var)
+            edges_filled[check] += 1
+            if edges_filled[check] == check_outdeg:
+                available_checks.remove(check)
+                full_checks.add(check)
     return H
 
-H = generate_coupled_h(N_VARS, N_CHECKS, VAR_OUTDEG, CHECK_OUTDEG, N_BLOCKS, COUPLING_WIDTH)
-for line in H:
-    print sum(line)
-np.savetxt("coupled_h.txt", H)
+H = generate_coupled_h(M, VAR_OUTDEG, CHECK_OUTDEG, N_BLOCKS, COUPLING_WIDTH)
+print H.shape, H.sum()
+np.save("coupled_h_large.npy", H)
             
